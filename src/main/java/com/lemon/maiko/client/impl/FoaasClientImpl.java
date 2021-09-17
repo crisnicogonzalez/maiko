2efcc9ab6fa7d351442ad0b33369d7d175af0cc3
cristian gonzalez <cristian3629@gmail.com>
2021-09-17 19:53:33 -0400

    comment

--- src/main/java/com/lemon/maiko/client/impl/FoaasClientImpl.java
+++ src/main/java/com/lemon/maiko/client/impl/FoaasClientImpl.java
@@ -2,8 +2,7 @@ package com.lemon.maiko.client.impl;
 
 import com.lemon.maiko.client.FoaasClient;
 import com.lemon.maiko.client.RestClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import javax.ws.rs.core.MediaType;
 
@@ -11,7 +10,6 @@ public class FoaasClientImpl implements FoaasClient {
 
     private final String host;
     private static final String COOL_ENDPOINT = "/cool/:from";
-    private static final Logger LOGGER = LoggerFactory.getLogger(FoaasClientImpl.class);
     private final RestClient restClient;
 
 
