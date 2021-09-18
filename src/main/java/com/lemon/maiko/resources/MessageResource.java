09392c35090d2c50931b9365738666415e48e78a
cristian gonzalez <cristian3629@gmail.com>
2021-09-18 14:11:11 -0400

    use userApiId header to pass foaas service

--- src/main/java/com/lemon/maiko/resources/MessageResource.java
+++ src/main/java/com/lemon/maiko/resources/MessageResource.java
@@ -4,6 +4,7 @@ import com.codahale.metrics.annotation.Timed;
 import com.lemon.maiko.core.services.MessageService;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -15,13 +16,15 @@ public class MessageResource {
 
     private MessageService messageService;
 
+    private static final String USER_API_ID_HEADER = "User-Api-Id";
+
     public MessageResource(MessageService messageService) {
         this.messageService = messageService;
     }
 
     @GET
     @Timed
-    public String getMessage() {
-        return this.messageService.getMessage("algo");
+    public String getMessage(@HeaderParam(USER_API_ID_HEADER) String userApiId) {
+        return this.messageService.getMessage(userApiId);
     }
 }
